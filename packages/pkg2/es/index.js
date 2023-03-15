deebbe208a30ebff6ec09c72040792a4e859f7b6
wangxiaoshylion <wangxiaoshylion@gmail.com>
2023-03-15 11:54:32 +0800

    feat: 提交

--- packages/pkg2/es/index.js
+++ packages/pkg2/es/index.js
@@ -1,6 +1,7 @@
 function add(a, b) {
   return a + b;
 }
+console.log('hello, 这是测试包pkg2');
 module.exports = {
-  add: add
+  add: add,
 };
--- packages/pkg2/lib/index.js
+++ packages/pkg2/lib/index.js
@@ -1,8 +1,9 @@
-"use strict";
+'use strict';
 
 function add(a, b) {
   return a + b;
 }
+console.log('hello, 这是测试包pkg2');
 module.exports = {
-  add: add
+  add: add,
 };
